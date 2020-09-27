a5dff32308db2597880b3e593488ef628b1eac3a
Akshath <akshathmahajan13@gmail.com>
2020-09-27 12:50:19 +0530

    Added auth system backend, finished project

--- EnigmaEncoder/core/urls.py
+++ EnigmaEncoder/core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home,output
-
+from .views import home,output,login, logout
 urlpatterns = [
     path('', home.as_view(), name="home"),
     path('output/', output.as_view(), name="output"),
+    path('login/', login.as_view(), name="login"),
+    path('logout/', logout.as_view(), name="logout"),
 ]
--- EnigmaEncoder/core/views.py
+++ EnigmaEncoder/core/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from .algorithms.decryption import decrypt
 from .algorithms.graph import find_node
 from .algorithms.get_marked import get_marked
 from .algorithms.data import g
 import json
 # Create your views here.
-class home(View):
+class home(LoginRequiredMixin, View):
+    login_url = reverse_lazy('login')
     def get(self, request):
         return render(request, 'core/index.html')
     def post(self, request):
@@ -24,7 +28,13 @@ class home(View):
         request.session['encrypted_text'] = encrypted_text
         request.session['location'] = location
         request.session['decrypted_text'] = decrypted_text
-        return redirect(reverse('output'))
-class output(View):
+        return redirect(reverse_lazy('output'))
+class output(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'core/output.html')
+
+class login(LoginView):
+    template_name = "core/login.html"
+
+class logout(LogoutView):
+    next_page = reverse_lazy('login')
